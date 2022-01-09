72531543ef18554e9e680ced23b46c199dbf5183
Martin Macharia <mosmartin5@gmail.com>
2022-01-10 00:03:58 +0100

    Update Register.jsx
    Update the register page.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,52 @@
+import { useState } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 export const Register = () => {
-    return (
-        <h1>
-            REGISTER PAGE
-        </h1>
-    )
-}
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target.value); 
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+
+        <h3>Login</h3>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
