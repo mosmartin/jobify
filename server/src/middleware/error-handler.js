a597f784a6fd11c8bf0b5445c6286366d95b542f
Martin Macharia <mosmartin5@gmail.com>
2022-01-10 23:55:17 +0100

    create the error handler middleware

---
+++ server/src/middleware/error-handler.js
@@ -0,0 +1,8 @@
+export const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+  });
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { errorHandler } from "./middleware/error-handler.js";
 import { notFound } from "./middleware/not-found.js";
 
 const app = express();
@@ -6,10 +7,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
+  throw new Error("💩😅");
   res.send("Hello World!");
 });
 
 app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`🚀 server listening on port ${port}...!`);
