2e01b7917bae23ad21054c802af5db80224694fd
Martin Macharia <mosmartin5@gmail.com>
2022-01-10 23:17:19 +0100

    Setup an express server

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 server listening on port ${port}...!`);
+});
