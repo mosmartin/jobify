013b5942286ce11c7e8d0b47635f2bb23140bb50
Martin Macharia <mosmartin5@gmail.com>
2022-01-10 21:34:04 +0100

    clear alerts

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useReducer } from "react";
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 import { reducer } from "./reducer";
 
 export const initialState = {
@@ -16,6 +16,14 @@ export const AppProvider = ({ children }) => {
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
+
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
   };
 
   return (
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,31 +1,24 @@
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 export const reducer = (state = {}, action) => {
-  //   switch (action.type) {
-  //     case "SET_LOADING":
-  //       return {
-  //         ...state,
-  //         isLoading: action.payload,
-  //       };
-  //     case "SHOW_ALERT":
-  //       return {
-  //         ...state,
-  //         showAlert: action.payload.showAlert,
-  //         alertMessage: action.payload.alertMessage,
-  //         alertType: action.payload.alertType,
-  //       };
-  //     default:
-  //       return state;
-  //   }
-
-  if (action.type === DISPLAY_ALERT) {
-    return {
-      ...state,
-      showAlert: true,
-      alertType: "danger",
-      alertMessage: "Please provide a valid inputs",
-    };
+  switch (action.type) {
+    case DISPLAY_ALERT:
+      return {
+        ...state,
+        showAlert: true,
+        // alertType: action.payload.alertType,
+        // alertMessage: action.payload.alertMessage,
+        alertType: "danger",
+        alertMessage: "Please provide a valid inputs",
+      };
+    case CLEAR_ALERT:
+      return {
+        ...state,
+        showAlert: false,
+        alertType: "",
+        alertMessage: "",
+      };
+    default:
+      return state;
   }
-
-  throw new Error(`Reducer not implemented for ${action.type}`);
 };
