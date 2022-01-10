f3c256274f87ecef118d96fcd211d00b379c3ea0
Martin Macharia <mosmartin5@gmail.com>
2022-01-10 23:45:34 +0100

    add the not found middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { notFound } from "./middleware/not-found.js";
 
 const app = express();
 
@@ -7,6 +8,8 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`🚀 server listening on port ${port}...!`);
